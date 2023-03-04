bbe848fa39534666f46e59e3789cd7c38e8d08da
Do Khac Duong <90135701+dokhacduong1@users.noreply.github.com>
2023-03-04 23:37:29 +0700

    Update-Done-Vip

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,21 @@
             section.scrollIntoView({behavior: 'smooth'});
         });
     });
+
+    var iconMenuOpen = document.querySelector("#menu");
+    var iconMenuClose = document.querySelector("#menu2");
+    var navHeader = document.querySelector("#navHeader");
+    var navHeaderLi = document.querySelectorAll("#navHeader li");
+    iconMenuOpen.onclick =function(){
+        iconMenuClose.classList.add("show-menu-pc2")
+        iconMenuOpen.classList.remove("menu-pc")
+        navHeader.classList.add("item-text-header-mobile");
+      
+        
+    }
+    iconMenuClose.onclick =function(){
+        iconMenuOpen.classList.add("menu-pc")
+        iconMenuClose.classList.remove("show-menu-pc2")
+        navHeader.classList.remove("item-text-header-mobile");
+    }
+    console.log(navHeaderLi);
